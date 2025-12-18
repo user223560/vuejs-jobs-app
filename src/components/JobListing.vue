561d4219588a3ce22052ca15fcd863a0166acffa
user223560 <122911711+user223560@users.noreply.github.com>
2025-12-19 01:29:35 +0400

    JobListing refactor

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,9 +1,23 @@
 <script setup>
-import { defineProps } from "vue"
+import { defineProps, ref, computed } from "vue"
 
-defineProps({
+const props = defineProps({
   job: Object,
 })
+
+const showFullDescription = ref(false)
+
+const truncatedDescription = computed(() => {
+  let description = props.job.description
+  if (!showFullDescription.value) {
+    description = description.substring(0, 100) + "..."
+  }
+  return description
+})
+
+const toggleFullDescription = () => {
+  showFullDescription.value = !showFullDescription.value
+}
 </script>
 <template>
   <div class="bg-white rounded-xl shadow-md relative">
@@ -14,7 +28,17 @@ defineProps({
       </div>
 
       <div class="mb-5">
-        {{ job.description }}
+        <div>
+          {{ truncatedDescription }}
+        </div>
+        <button
+          class="text-green-500 hover:text-green-600 mb-5"
+          @click="toggleFullDescription"
+        >
+          {{
+            showFullDescription ? "Свернуть описание" : "Развернуть описание"
+          }}
+        </button>
       </div>
 
       <h3 class="text-green-500 mb-2">{{ job.salary }}</h3>
@@ -23,7 +47,7 @@ defineProps({
 
       <div class="flex flex-col lg:flex-row justify-between mb-4">
         <div class="text-orange-700 mb-3">
-          <i class="fa-solid fa-location-dot text-lg"></i>
+          <i class="bi bi-geo-alt text-orange-700"></i>
           {{ job.location }}
         </div>
         <a
