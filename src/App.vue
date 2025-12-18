24b19ccec3c78d1e2469b92f838cae159abab4ca
user223560 <122911711+user223560@users.noreply.github.com>
2025-12-18 19:19:31 +0400

    Cards container added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue"
 import Hero from "@/components/Hero.vue"
+import HomeCards from "@/components/HomeCards.vue"
 </script>
 
 <template>
   <Navbar />
   <Hero />
+  <HomeCards />
 </template>
 
 <style scoped></style>
---
+++ src/components/HomeCards.vue
@@ -0,0 +1,32 @@
+<script setup></script>
+<template>
+  <section class="py-4">
+    <div class="container-xl lg:container m-auto">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+        <div class="bg-gray-100 p-6 rounded-lg shadow-md">
+          <h2 class="text-2xl font-bold">Для соискателей</h2>
+          <p class="mt-2 mb-4">Легкий и удобный поиск вакансий</p>
+          <a
+            href="jobs.html"
+            class="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+          >
+            Найти вакансию
+          </a>
+        </div>
+        <div class="bg-green-100 p-6 rounded-lg shadow-md">
+          <h2 class="text-2xl font-bold">Для работодателей</h2>
+          <p class="mt-2 mb-4">
+            Размести свою вакансию для подбора лучшего специалиста
+          </p>
+          <a
+            href="add-job.html"
+            class="inline-block bg-green-500 text-white rounded-lg px-4 py-2 hover:bg-green-600"
+          >
+            Разместить вакансию
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped></style>
