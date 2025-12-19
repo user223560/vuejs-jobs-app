20396009e292bbece5161e33e3130d84a2115b0a
user223560 <122911711+user223560@users.noreply.github.com>
2025-12-19 16:15:59 +0400

    Navbar navigation added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { RouterLink } from "vue-router"
+
 import logo from "@/assets/img/logo.png"
 </script>
 
@@ -9,29 +11,32 @@ import logo from "@/assets/img/logo.png"
         <div
           class="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
         >
-          <a class="flex flex-shrink-0 items-center mr-4" href="index.html">
+          <RouterLink class="flex flex-shrink-0 items-center mr-4" to="/">
             <img class="h-10 w-auto" :src="logo" alt="ИщуРаботу.ру" />
             <span class="hidden md:block text-white text-2xl font-bold ml-2"
               >ИщуРаботу.ру</span
             >
-          </a>
+          </RouterLink>
           <div class="md:ml-auto">
             <div class="flex space-x-2">
-              <a
-                href="index.html"
+              <RouterLink
+                to="/"
                 class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >Главная</a
               >
-              <a
-                href="jobs.html"
+                Главная
+              </RouterLink>
+              <RouterLink
+                to="/jobs"
                 class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
-                >Вакансии</a
               >
-              <a
-                href="add-job.html"
+                Вакансии
+              </RouterLink>
+              <RouterLink
+                to="/jobs/add"
                 class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
-                >Добавить вакансию</a
               >
+                Добавить вакансию
+              </RouterLink>
             </div>
           </div>
         </div>
