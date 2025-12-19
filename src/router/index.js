b7684c29d3ecbbd8789a86aee7d989936b1ef083
user223560 <122911711+user223560@users.noreply.github.com>
2025-12-19 18:29:42 +0400

    404 page added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router"
+
 import HomeView from "@/views/HomeView.vue"
 import JobsView from "@/views/JobsView.vue"
+import NotFoundView from "@/views/NotFoundView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,11 @@ const router = createRouter({
       path: "/jobs",
       name: "jobs",
       component: JobsView
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "not-found",
+      component: NotFoundView
     }
   ]
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink } from "vue-router"
+</script>
+
+<template>
+  <section class="text-center flex flex-col justify-center items-center h-96">
+    <i class="bi bi-exclamation-triangle text-yellow-500 text-7xl mb-5"></i>
+    <h1 class="text-6xl font-bold mb-4">Ошибка 404</h1>
+    <p class="text-xl mb-5">Эта страница была удалена или перемещена</p>
+    <RouterLink
+      to="/"
+      class="text-white bg-green-700 hover:bg-green-900 rounded-md px-3 py-2 mt-4"
+    >
+      Назад
+    </RouterLink>
+  </section>
+</template>
+
+<style scoped></style>
